12eb8b10c37bbb4f25749ec7016f8d1a7b6572a4
Sam Larison <samlarison@gmail.com>
2014-07-04 12:37:23 -0500

    support for form input arrays

--- copyform.user.js
+++ copyform.user.js
@@ -8,12 +8,19 @@ function(request, sender, sendResponse) {
 	if(request.pasteform){
 		delete request.pasteform;
 		for( i in request ){
-			var n=parentForm.querySelector("[name='"+i+"']");
-			if( n ){
-				if( n.type == 'checkbox' ){
-					n.checked = request[i];
-				}else
-					n.value = request[i];
+			var vals=request[i];
+			var valsIsArray = typeof(vals) == 'object';
+			var elms=parentForm.querySelectorAll("[name='"+i+"']"), n, v;
+			for( var frmelm=0,l=elms.length; frmelm<l; frmelm++ ){
+				n = elms[frmelm];
+				v = valsIsArray ? vals[frmelm] : vals;
+				typeof(v)
+				if( n ){
+					if( n.type == 'checkbox' ){
+						n.checked = v;
+					}else
+						n.value = v;
+				}
 			}
 		}
 		sendResponse({});
@@ -28,12 +35,25 @@ function(request, sender, sendResponse) {
 
 function appendFrmElms(resp, formElms){
 	for(var f=0,l=formElms.length; f<l; f++){
+		var newValue = null
 		if( formElms[f].type == 'hidden' ){
 			//skip
 		}else if( formElms[f].type == 'checkbox' ){
-			resp[formElms[f].name] = formElms[f].checked;
-		}else
-			resp[formElms[f].name] = formElms[f].value;
+			newValue = formElms[f].checked;
+		}else{
+			newValue = formElms[f].value;
+		}
+
+		if( newValue !== null ){
+			if( resp[formElms[f].name] ){
+				if(typeof(resp[formElms[f].name]) != 'object'){
+					resp[formElms[f].name] = [resp[formElms[f].name]];
+				}
+				resp[formElms[f].name].push(newValue);
+			}else{
+				resp[formElms[f].name] = newValue;
+			}
+		}
 	}
 	return resp;
 }
